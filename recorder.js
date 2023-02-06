0afe548b28c1cad1977a03feb8ffc3a9fc58396c
Damien Cruz <93546644+not-damien@users.noreply.github.com>
2023-02-06 15:34:49 -0800

    added get perm function from prototype

--- recorder.js
+++ recorder.js
@@ -14,3 +14,37 @@ $("#start").mouseleave(function(){
     console.log('bye');
     $(this).attr("src","Images/Start Recording_.svg")
 })
+
+ async function getPerm(){
+        const stream = await navigator.mediaDevices.getDisplayMedia({video: true, audio: true});
+        var options = {mimeType: 'video/webm; codecs=vp9'};
+        const recorder = new MediaRecorder(stream, options);
+
+
+        const chunks = [];
+        recorder.ondataavailable = (e) =>
+        chunks.push(e.data);
+
+        recorder.onstop = () => {
+            const track = stream.getVideoTracks()[0];
+            track.stop();
+
+            const blob = new Blob(chunks, {type: chunks[0].type})
+
+            const a = 
+            window.document.createElement("a");
+            
+            //pop up to ask user for file name
+            let filename =   "recording.mp4"
+                //window.prompt("enter a name to save your file", "recording.mp4")
+            
+            a.href = URL.createObjectURL(blob);
+            if(filename != null){
+                a.download = filename;
+            }else{
+                a.download = "recording.mp4"
+            }
+
+            document.body.appendChild(a);
+            $('a').html("download here")
+        }
